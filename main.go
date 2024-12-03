56a10d095c61565d2a562fd5c83ef3d1fa6fd351
Marcos Lilljedahl <marcosnils@gmail.com>
2024-12-03 14:07:07 -0300

    pla
    Signed-off-by: Marcos Lilljedahl <marcosnils@gmail.com>

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/coreos/go-oidc/v3/oidc"
+)
+
+func main() {
+
+	ctx := context.Background()
+
+	keySet := oidc.NewRemoteKeySet(ctx, "https://token.actions.githubusercontent.com/.well-known/jwks")
+	verifier := oidc.NewVerifier("https://token.actions.githubusercontent.com", keySet, &oidc.Config{SkipClientIDCheck: true})
+
+	t, err := verifier.Verify(ctx, os.Getenv("ACTIONS_ID_TOKEN_REQUEST_TOKEN"))
+
+	fmt.Printf("Result is: %+v - %v\n", t, err)
+
+}
