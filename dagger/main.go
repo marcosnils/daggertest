6ae822dc67aeff8f3a66fd50c6cad7cc80f391e8
Marcos Lilljedahl <marcosnils@gmail.com>
2026-02-19 15:19:18 -0300

    trigger
    Signed-off-by: Marcos Lilljedahl <marcosnils@gmail.com>

--- dagger/main.go
+++ dagger/main.go
@@ -20,7 +20,6 @@ import (
 
 type Daggertest struct{}
 
-// Returns a container that echoes whatever string argument is provided
 func (m *Daggertest) ContainerEcho(stringArg string) *dagger.Container {
 	return dag.Container().From("alpine:latest").WithExec([]string{"echo", stringArg})
 }
